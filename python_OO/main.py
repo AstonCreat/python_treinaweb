55537693beb1abc41951868bfe72fe1d539d3598
italo Araujo <italoasousa@hotmail.com>
2018-12-02 11:31:29 -0200

    resolvido

--- python_OO/main.py
+++ python_OO/main.py
@@ -1,8 +1,8 @@
 import python_OO.carro
-#import python_OO.moto
+import python_OO.moto
 
 up = python_OO.carro.Carro("vermelho", "flex", 1.0, "VW", 4)
-upCross = python_OO.carro.Carro("Azul", "flex", 1.0, "VW", 4)
+
 
 
 up.abastecer(50)
@@ -10,8 +10,9 @@ up.abastecer(100)
 up.abastecer(5)
 up.acelerar()
 
+moto_ninja = python_OO.moto.Moto("verde", "gasolina", 600, "ninja", 2)
 
-#moto_ninja = python_OO.moto.Moto("verde", "gasolina", 600, "ninja", 2)
+moto_ninja.ligar()
 
-#moto_ninja.ligar()
-#print(moto_ninja.is_ligado)
+print(f"moto: {moto_ninja.passageiro}")
+print(moto_ninja.is_ligado)
--- python_OO/veiculo.py
+++ python_OO/veiculo.py
@@ -1,5 +1,5 @@
-class Veiculo:
-    def __init__(self, cor,  tipo_combustivel, potencia, marca):
+class Veiculo():
+    def __init__(self, cor, tipo_combustivel, potencia, marca):
         self.cor = cor
         self.tipo_combustivel = tipo_combustivel
         self.potencia = potencia
@@ -8,30 +8,30 @@ class Veiculo:
         self.is_ligado= False
         self.velocidade = 0
 
-        def __del__(self):
-            print("obj foi limpado da memoria")
+    def __del__(self):
+        print("obj foi limpado da memoria")
 
-        def abastecer(self, qtd_combustivel):
-            self.qtd_combustivel += qtd_combustivel
+    def abastecer(self, qtd_combustivel):
+        self.qtd_combustivel += qtd_combustivel
 
-        def ligar(self):
-            if self.is_ligado:
-                print("O veiculo já está ligado")
-            else:
-                self.is_ligado = True
-                print("veiculo foi ligado")
+    def ligar(self):
+        if self.is_ligado:
+            print("O veiculo já está ligado")
+        else:
+            self.is_ligado = True
+            print("veiculo foi ligado")
 
-        def desliga(self):
-            if self.is_ligado == False:
-                print("O veiculo já está desligado")
-            else:
-                self.is_ligado = False
-                print("veiculo foi desligado")
+    def desliga(self):
+        if self.is_ligado == False:
+            print("O veiculo já está desligado")
+        else:
+            self.is_ligado = False
+            print("veiculo foi desligado")
 
-        def acelerar(self, velocidade=10):
-            if self.is_ligado:
-                self.velocidade += velocidade
-                print("velocidade")
-                print(f"{velocidade} km/Hr")
-            else:
-                print("Precisa está ligado para acelerar");
+    def acelerar(self, velocidade=10):
+         if self.is_ligado:
+            self.velocidade += velocidade
+            print("velocidade")
+            print(f"{velocidade} km/Hr")
+         else:
+            print("Precisa está ligado para acelerar");
